da8988b56888835889996be672b31368baa4fa21
dayanyan110 <atae.t1126106@ezweb.ne.jp>
2019-06-14 21:57:15 +0900

    tama make html2

--- templates/main.js
+++ templates/main.js
@@ -1,7 +1,20 @@
 var app = new Vue({
     el: '.main',
+    data:{      //ここで定義した値がv-model="hoge"や{{hoge}}の初期値に反映される
+        url:"8.8.8.8",//v-model="url"の初期値
+        param:"{'sec':'4'}",             //v-model="param"の初期値
+       },
     methods: {
       handleClick: function (event) {
+        config = {
+            headers:{
+              'X-Requested-With': 'XMLHttpRequest',
+              'Content-Type':'application / x-www-form-urlencoded'
+            },
+            withCredentials:true,
+          }
+        //  param = JSON.parse(this.param)
+          axios.post(this.url,this.param,config)
         alert('Ajaxを投げるよ') // [object HTMLButtonElement]
       }
     }

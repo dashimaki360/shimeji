9fc3e3f482a0b42178c38a23073f4309c4d5f6f8
Your Name <you@example.com>
2019-06-14 14:10:05 +0000

    finish

--- app.py
+++ app.py
@@ -16,7 +16,25 @@ class Shimeji():
         self.initialize()
 
     def initialize(self):
-        pass
+        GPIO.setmode(GPIO.BCM)
+
+        #GPIO4を制御パルスの出力に設定
+        self.gp_servo_out = 4
+        self.gp_mist_out = 15
+       
+        GPIO.setup(self.gp_servo_out, GPIO.OUT)
+        GPIO.setup(self.gp_mist_out, GPIO.OUT)
+        
+        #「GPIO4出力」でPWMインスタンスを作成する。
+        #GPIO.PWM( [ピン番号] , [周波数Hz] )
+        #SG92RはPWMサイクル:20ms(=50Hz), 制御パルス:0.5ms〜2.4ms, (=2.5%〜12%)。
+        self.servo = GPIO.PWM(self.gp_servo_out, 50)
+        
+        #パルス出力開始。　servo.start( [デューティサイクル 0~100%] )
+        #とりあえずゼロ指定だとサイクルが生まれないので特に動かないっぽい？
+        #servo.start(0)
+        
+        GPIO.output(self.gp_mist_out, GPIO.LOW)
 
     def to_json(self):
         json_dict = {
@@ -30,7 +48,10 @@ class Shimeji():
         # TODO
         # blow mist during sec(sec), after passed sec time turn off light.
         # if sec = 0, No mist.
-        pass
+        GPIO.output(self.gp_mist_out, GPIO.HIGH)
+        time.sleep(sec)
+        GPIO.output(self.gp_mist_out, GPIO.LOW)
+        
 
     def light_up(self, sec=0):
         # light up during sec(sec), after passed sec time turn off light.
@@ -71,17 +92,21 @@ def shimeji_controller(action_type, param):
 
 @app.route('/action', methods=["GET", "POST"])
 def action():
+    print("get request")
     if request.method == "GET":
         return shimeji.to_json()
     elif request.method == "POST":
+        print("post req")
         data = request.json
-        return shimeji_controller(data["type"], data["param"])
+        shimeji.blow_mist(4)
+        # return shimeji_controller(data["type"], data["param"])
 
 
 @app.route('/')
 def root():
+    print("hoge")
     return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- static/main.js
+++ static/main.js
@@ -1,19 +1,16 @@
 var app = new Vue({
     el: '.main',
     data:{      //ここで定義した値がv-model="hoge"や{{hoge}}の初期値に反映される
-        url:"127.0.0.1:5000/action",//v-model="url"の初期値
-        param:"{'type': 'mist',
-                 'param': {'sec':'4'}
-               }",             //v-model="param"の初期値
+        url:"http://localhost:5000/action",//v-model="url"の初期値
+        param:"{'sec':'4'}", //v-model="param"の初期値
     },
     methods: {
       handleClick: function (event) {
         config = {
             headers:{
-              'X-Requested-With': 'XMLHttpRequest',
-              'Content-Type':'application / x-www-form-urlencoded'
+              'Content-Type':'application/x-www-form-urlencoded;application/json;charset=utf-8'
             },
-            withCredentials:true,
+            //withCredentials:true,
           }
         //  param = JSON.parse(this.param)
           axios.post(this.url,this.param,config)
